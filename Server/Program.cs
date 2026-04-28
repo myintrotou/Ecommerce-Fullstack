a385b19fa4bb482caab974f680f9c2ea535db869
myintrotou <hs556064@gmail.com>
2026-04-28 12:24:05 +0530

    Fix: Add automatic migrations on startup for cloud deployment

--- Server/Program.cs
+++ Server/Program.cs
@@ -83,6 +83,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate(); // This creates the tables automatically on the server
     DbSeeder.Seed(context);
 }
 
